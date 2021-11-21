11737b0c6910844b1459b8eec1aa9c526e83341b
tsaniamagistra <90882282+tsaniamagistra@users.noreply.github.com>
2021-11-21 09:03:28 +0700

    Melengkapi fungsi hitungpembelian

--- Proyek-semester1.cpp
+++ Proyek-semester1.cpp
@@ -62,8 +62,6 @@ int main(){
 	else if (menu == 3){
 		cout<<"\nHitung Pembelian"<<endl;
 		hitungpembelian();
-		
-		
 		}
 	else{
 	cout<<"Input Tidak Sesuai"<<endl;	
@@ -71,11 +69,14 @@ int main(){
 	}
 
 void hitungpembelian(){
-	int jml_beli, jumlah;
+	string barang[6] = {{"Sabun Mandi"},{"Minyak Goreng"},{"Sikat Gigi"},{"Pasta Gigi"},{"Mie Instan"},{"Tissue"}};
+	int harga[6] = {{5000},{12000},{4000},{7000},{3000},{10000}};
+	int jml_beli, jumlah, hargasatuan, total;
 	string nama_barang;
 	char tambah;
 	
 	jml_beli = 1;
+	total = 0;
 	for (int i=0; i<jml_beli; i++){
 		cout<<endl;
 		cout<<"Masukkan Barang Ke-"<<i+1<<endl;
@@ -88,16 +89,45 @@ void hitungpembelian(){
 		cin>>jumlah;
 		
 		cout<<"Harga Satuan\t: ";
-		//harga sesuai nama barang yg diinput?
+		if (nama_barang == barang[0]){
+			hargasatuan = harga[0];
+			cout<<hargasatuan;
+		}
+		else if (nama_barang == barang[1]){
+			hargasatuan = harga[1];
+			cout<<hargasatuan;
+		}
+		else if (nama_barang == barang[2]){
+			hargasatuan = harga[2];
+			cout<<hargasatuan;
+		}
+		else if (nama_barang == barang[3]){
+			hargasatuan = harga[3];
+			cout<<hargasatuan;
+		}
+		else if (nama_barang == barang[4]){
+			hargasatuan = harga[4];
+			cout<<hargasatuan;
+		}
+		else if (nama_barang == barang[5]){
+			hargasatuan = harga[5];
+			cout<<hargasatuan;
+		}
+		else {
+			hargasatuan = 0;
+			cout<<"Harga Barang Tidak Ditemukan";
+		}
 		
-		cout<<"\nSub Total\t: \n";
-		//sub_total = jumlah*harga
+		int subtotal[50] = {0};
+		subtotal[i] = (jumlah*hargasatuan);
+		total += subtotal[i];
+		cout<<"\nSub Total\t: "<<subtotal[i];
 		
-		cout<<"Tambah Barang (Y/N)? "; cin>>tambah;
+		cout<<"\nTambah Barang (Y/N)? "; cin>>tambah;
 		if (tambah == 'Y' || tambah == 'y') jml_beli++;
 		else if (tambah == 'N' || tambah == 'n'){
 			cout<<"\nTotal\t: ";
-			//cout<<total;
+			cout<<total;
 		}
 	}
 }
